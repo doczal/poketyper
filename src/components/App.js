2a191377ceb41fe373c7728d3518175b892bfccb
Rizal <doczal@gmail.com>
2018-12-25 17:05:44 +0800

    Session handling

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,21 @@ import LandingPage from './Landing';
 import SignUpPage from './SignUp';
 import Game from './Game'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import * as ROUTES from '../constants/routes';
+import { withAuthentication } from './Session';
 
-class App extends Component {
+const App = () => (
 
-  render() {
-    
-    return (
-      <Router>
-        <div className="App">
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-          <Route exact path={ROUTES.SIGN_IN} component={LandingPage} />
-          <Route exact path={ROUTES.GAME} component={Game} />
-        </div>
-      </Router>
-    );
-  }
-}
+  <Router>
+    <div className="App">
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={LandingPage} />
+      <Route exact path={ROUTES.GAME} component={Game} />
+    </div>
+  </Router>
 
-export default App;
+);
+
+export default withAuthentication(App);
