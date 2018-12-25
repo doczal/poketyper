c7def8201a3ac637d4f7df0956539bf6cfd89663
Rizal <doczal@gmail.com>
2018-12-25 12:16:07 +0800

    Set up SignIn component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignIn from './SignIn';
 import * as ROUTES from '../constants/routes';
 
 const Landing = () => (
   <div>
     <h1>Landing</h1>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.GAME}>Play as Guest</Link>
-      </li>
-    </ul>
+    <SignIn />
+    <Link to={ROUTES.GAME}>Play as Guest</Link>
   </div>
 )
 
