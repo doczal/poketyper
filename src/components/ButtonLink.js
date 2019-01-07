fd263e8c297e92bf984430dac1c99d2b9eb44ecf
Rizal <doczal@gmail.com>
2019-01-07 21:22:50 +0800

    Edit ButtonLink to not use an actual button element

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -3,9 +3,7 @@ import { Link } from 'react-router-dom';
 import '../styles/ButtonLink.scss';
 
 const ButtonLink = ({to, children, className=""}) => (
-  <button type="button" className={`ButtonLink ${className}`}>
-    <Link to={to}>{children}</Link>
-  </button>
+  <Link className={`ButtonLink ${className}`} to={to}>{children}</Link>
 )
 
 export default ButtonLink;
